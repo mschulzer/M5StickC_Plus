01ceba3f69bae37c4dd9a72dfc3bee4e10afed08
Morten Schultz <45948423+mschulzer@users.noreply.github.com>
2022-11-13 22:14:28 +0100

    Create button_example.py

---
+++ button_example.py
@@ -0,0 +1,15 @@
+from micropython import const
+from ubutton import Button
+
+_BUTTON_A_PIN = const(39)
+_BUTTON_B_PIN = const(38)
+_BUTTON_C_PIN = const(37)
+
+m5button = Button()
+buttonA = m5button.register(_BUTTON_A_PIN)
+buttonB = m5button.register(_BUTTON_B_PIN)
+buttonC = m5button.register(_BUTTON_C_PIN)
+
+while True:
+  if buttonC.wasPressed():
+    print("Hello")
