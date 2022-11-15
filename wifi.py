a4f0e6d3a79401d63f7dbf92ae0ee7ceda1b3702
Morten Schultz <45948423+mschulzer@users.noreply.github.com>
2022-11-15 18:14:01 +0100

    Update wifi.py

--- wifi.py
+++ wifi.py
@@ -2,7 +2,7 @@ import network
 
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
-wlan.scan()
+#wlan.scan()
 wlan.connect('ssid', 'key')
-wlan.isconnected()
-wlan.ifconfig()
+wlan.isconnected() # Check whether you're connected
+wlan.ifconfig()    # Get IP-address
