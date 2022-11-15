e7e0f7fbcd86574dafdddf2e183eaf5e6d4bf464
Morten Schultz <45948423+mschulzer@users.noreply.github.com>
2022-11-15 18:14:36 +0100

    Update display.py

--- display.py
+++ display.py
@@ -10,6 +10,7 @@ spi = machine.SPI(2, baudrate=20000000, polarity=1, sck=machine.Pin(13, machine.
 display = st7789.ST7789(spi, 135, 240, rotation=3, reset=machine.Pin(18, machine.Pin.OUT), dc=machine.Pin(23, machine.Pin.OUT), cs=machine.Pin(5, machine.Pin.OUT))
 
 display.init()
+
 display.fill(st7789.WHITE)
 display.text(font, "Hello, World!", 50, 80, 0x000000, 0xFFFFFF)
 
